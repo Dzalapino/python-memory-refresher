a0f9273cebef1f9aed3535d02d541fa29d2765c6
Dzalapino <224407@edu.p.lodz.pl>
2023-02-20 23:44:22 +0100

    After classes

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 from calculator import add
 
 print(add(1, 2))
+
+class Phone:
+
+    def __init__(self, brand, price):
+        self.brand = brand
+        self.price = price
+
+    def call(self, phoneNumber):
+        print(f"{self.brand} is calling {phoneNumber}")
+
+    def __str__(self) -> str:
+        return f"Brand: {self.brand}\nprice: {self.price}"
+
+iphone = Phone("Iphone 7+", 300)
+motorola = Phone("motorola moto", 210)
+
+print(iphone)
+print(motorola)
+iphone.call(112)
