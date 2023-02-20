a8eecbb3cdd9b36795e32efcbbdbe6563267129d
Dzalapino <224407@edu.p.lodz.pl>
2023-02-20 23:12:50 +0100

    After loops

--- general.py
+++ general.py
@@ -11,5 +11,34 @@ else:
 message = "positive" if number > 0 else "0 or negative"
 print(message)
 
-# oe:
+# or:
 print("positive" if number > 0 else "0 or negative")
+
+
+names = ["Jacob", "Gabrielle", "Genevieve", "Lucas"]
+
+# for loops
+for name in names:
+    print(name.upper())
+
+# loop through dictionaries
+person = {
+    "name": "Jamal",
+    "age": 23,
+    "address": "Sezamkowa 420"
+}
+
+for key in person:
+    print(f"key: {key}, value: {person[key]}")
+
+for key, value in person.items():
+    print(f"key: {key}, value: {value}")
+
+# while loop
+number = 0
+
+while number < 5:
+    number += 2
+    print(number)
+else:
+    print("While loop ended")
